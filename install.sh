5869b80bedee67f81915697c01ceddbc2fa254a7
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-01-04 00:05:52 -0900

    point toward official nodevalet repo so I don't break anything

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # One Master Script
 
-cd /var/tmp ; sudo rm -rf nodevalet ; sudo git clone https://github.com/akcryptoguy/nodevalet -b master; cd nodevalet; sudo bash silentinstall.sh
+cd /var/tmp ; sudo rm -rf nodevalet ; sudo git clone https://github.com/nodevalet/nodevalet -b master; cd nodevalet; sudo bash silentinstall.sh
